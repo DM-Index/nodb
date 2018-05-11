ab96ba160e784a07dfe7aa2f773c48b1e43e7e34
Nathan <n.hatz@yahoo.com>
2018-05-11 15:39:03 -0500

    Initial modifications complete, app now represents base state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./base_styles/App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  componentDidMount() {}
+
   render() {
     return <div className="App" />;
   }
