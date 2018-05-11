37362cf6fe57dffed674547a0a4e611c0af01307
Nathan <n.hatz@yahoo.com>
2018-05-11 15:34:19 -0500

    Removed more bloat and reoganized files

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { json } = require("body-parser");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
 
 app.use(json());
-
+app.use(cors());
 
 //app.get data
 
@@ -16,5 +17,4 @@ app.use(json());
 
 //app.delete data
 
-app.listen(port, () =>
-console.log(`Now Listening To Port:${port}FM`))
+app.listen(port, () => console.log(`Now Listening To Port:${port}FM`));
